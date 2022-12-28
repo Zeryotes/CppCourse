8c6010f59dc2c30f297be8d757fc3b377ac749b0
Jordão de Souza Lopes <jordaodesouzalopes@gmail.com>
2022-12-28 09:07:09 -0300

    Aula 1 - iostream, cout e endl

---
+++ 1. Primeiro Programa em C++/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* CONTEÚDO DA SEÇÃO:
+** - Biblioteca padrão: #include <iostream>
+** - Saída de dados: std::cout e std::endl;
+**
+**
+*/
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << "Number 1" << std::endl;
+    std::cout << "Number 2" << std::endl;
+    std::cout << "Number 3" << std::endl;
+
+    std::cout << "--------" << std::endl;
+    
+    std::cout << "Number 1";
+    std::cout << "Number 2";
+    std::cout << "Number 3";
+    return 0;
+}
