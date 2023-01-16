b22f7a350de42fd75c8207f518ee95a7d6d1e59a
Jordão de Souza Lopes <jordaodesouzalopes@gmail.com>
2023-01-16 19:46:27 -0300

    Aula 12 - Booleanos

---
+++ 12. Booleans/Notas.md
@@ -0,0 +1,9 @@
+# Booleans
+
+Tipo de dado que pode armazenar dois valores True (verdadeiro) ou False (falso).
+
+1 - True
+
+0 - False
+
+Boolalpha
--- 12. Booleans/main.cpp
+++ 12. Booleans/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
 /* CONTEÚDO DA SEÇÃO:
-** - 
+** - Booleans
 */
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    bool luz_vermelha{true};
+    bool luz_verde{false};
 
+    if(luz_vermelha == true){
+        std::cout << "PARE!" << std::endl;
+    } else {
+        std::cout << "Siga em frente!" << std::endl;
+    }
+    
+    // Sizeof()
+    std::cout << "Sizeof(bool) : " << sizeof(bool) << std::endl;
+
+    // Imprimindo um booleano
+    // 1 -> true
+    // 2 -> false
+
+    std::cout << std::endl;
+    std::cout << "Luz vermelha : " << luz_vermelha << std::endl;
+    std::cout << "Luz verde : " << luz_verde << std::endl;
+
+    std::cout << std::endl;
+    // Outra maneira de imprimir um booleano de maneira mais clara
+    std::cout << std::boolalpha;
+    std::cout << "Luz vermelha : " << luz_vermelha << std::endl;
+    std::cout << "Luz verde : " << luz_verde << std::endl;
     return 0;
 }
