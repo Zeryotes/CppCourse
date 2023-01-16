691d61ba28750d4a47adbdfa60dd06cb40aaa4ed
Jordão de Souza Lopes <jordaodesouzalopes@gmail.com>
2023-01-16 19:58:11 -0300

    Aula 13 - Caracteres e Textos

---
+++ 13. Textos e Caracteres/Notas.md
@@ -0,0 +1,10 @@
+# Caracteres e Textos
+
+char é a palavra reservada para instanciar um caractere.
+
+Também é necessário definir um caractere com aspas simples ' ' para não receber um erro de compilação.
+
+Tabela ASCII é a maneira mais convencional de codificar um caractere.
+
+Comando para imprimir o valor inteiro ao invés do caractere:
+### static_cast<int>(valor)
---
+++ 13. Textos e Caracteres/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/* CONTEÚDO DA SEÇÃO:
+** - Caracteres (CHAR)
+** - Textos (STRING)
+*/
+
+int main(){
+    char caractere1 {'c'};
+    char caractere2 {'a'};
+    char caractere3 {'r'};
+    char caractere4 {'r'};
+    char caractere5 {'o'};
+
+    std::cout << caractere1 << std::endl;
+    std::cout << caractere2 << std::endl;
+    std::cout << caractere3 << std::endl;
+    std::cout << caractere4 << std::endl;
+    std::cout << caractere5 << std::endl;
+
+    // Um byte na memória : 2^8 = 256 valores diferentes (0 ~ 255)
+    std::cout << std::endl;
+
+    char valor = 65; // Código para 'A' na tabela ASCII
+    std::cout << "Valor : " << valor << std::endl;
+    std::cout << "Valor (int) : " << static_cast<int>(valor) << std::endl;
+
+
+    return 0;
+}
